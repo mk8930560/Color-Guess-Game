ccdd38749500635b349f2086423892a2a0445886
mk8930560 <parasermanish@gmail.com>
2020-11-08 16:42:32 +0530

    Color Guess Game

---
+++ script.js
@@ -0,0 +1,46 @@
+var heading = document.getElementById("colourValue");
+
+var buttons = document.getElementsByClassName("colourButton");
+
+var answerMessage = document.getElementById("answer");
+
+function startGame()
+{
+	answerMessage.innerHTML = "";
+
+	var answerButton = Math.round(Math.random()*(buttons.length-1));
+
+	for(var i=0;i<buttons.length;i++)
+	{
+		var red = makeColourValue();
+		var green = makeColourValue();
+		var blue = makeColourValue();
+
+		setButtonColour(buttons[i],red,green,blue);
+
+		if(i === answerButton){
+			heading.innerHTML = '('+red+', '+green+', '+blue+')';
+		}
+
+		buttons[i].addEventListener("click",function(){
+			if(this===buttons[answerButton]){
+				answerMessage.innerHTML = "Correct!";
+			}else{
+				answerMessage.innerHTML = "Wrong answer! Guess Again!";
+			}
+		});
+	}
+}
+
+function setButtonColour(button,red,green,blue) {
+	button.setAttribute('style',
+		'background-color:rgb('+red+','+green+','+blue+');');
+}
+
+function makeColourValue() {
+	return Math.round(Math.random()*255);
+}
+
+startGame();
+
+document.getElementById('resetButton').addEventListener('click',startGame);
